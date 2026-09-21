ac6cb85e84b230b37457000698fdf7726333c595
gmveronesi-cloud <gmveronesi@gmail.com>
2026-09-21 15:08:12 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+// Valori PUBBLICI del progetto Supabase (sicuri nel browser).
+// NON mettere mai qui la Secret key / service_role.
+window.WB_CONFIG = {
+  supabaseUrl: 'https://rxxqdseojllcniowrzxm.supabase.co',
+  supabaseKey: 'sb_publishable_ZgDUzUKnqTDbzPLHhpd3bA_vXuB1lrO',
+};
